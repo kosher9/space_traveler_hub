300dcc9d3a79c982ca1921161c15d370e296f376
kosher <lionel.kossi@gmail.com>
2022-05-10 09:42:10 +0000

    Create empty navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
 
-export const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <Navbar />
+  );
 }
+
+export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => (
+  <header>
+    <nav>
+      <NavLink to="/">Home</NavLink>
+    </nav>
+  </header>
+);
+
+export default Navbar;
