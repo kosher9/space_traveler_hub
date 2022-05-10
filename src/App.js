4a0cb851174f5f478bd0b76d26a7330dd30055e0
levy002 <levy.ukwi002@gmail.com>
2022-05-10 13:08:19 +0200

    Creating Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </>
   );
 }
 
