9a2ed57ee3c683a93c86e992d7dcb5cc7fcffb93
levy002 <levy.ukwi002@gmail.com>
2022-05-11 19:33:09 +0200

    Dispatching actions for joining or leaving mission and render the changes on the page

--- src/components/misssions/Mission.js
+++ src/components/misssions/Mission.js
@@ -1,14 +1,13 @@
-/* eslint-disable camelcase */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getMissions } from '../../redux/missions/Missions';
+import { getMissions, joiningMissionAction, leavingMissionAction } from '../../redux/missions/Missions';
 
 const Mission = () => {
-  const Missions = useSelector((state) => state.missions);
+  const missions = useSelector((state) => state.missions);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (Missions.length === 0) {
+    if (missions.length === 0) {
       dispatch(getMissions());
     }
   }, []);
@@ -16,21 +15,47 @@ const Mission = () => {
   return (
     <>
       {
-        Missions.map(({
-          mission_name,
-          mission_id,
-          description,
-        }) => (
-          <tr key={mission_id} className="border border-slate-400">
+        missions.map((msn) => (
+          <tr key={msn.id} className="border border-slate-400">
             <th className="text-left text-base flex p-2">
-              <p>{mission_name}</p>
+              <p>{msn.name}</p>
             </th>
-            <td className="border border-slate-400 text-sm p-2 pb-5">{description}</td>
+            <td className="border border-slate-400 text-sm p-2 pb-5">{msn.description}</td>
             <td className="border border-slate-400 p-1">
-              <p className="text-sm text-white bg-gray-600 text-center w-28 border rounded">NOT A MEMBER</p>
+              {msn.reserved
+                ? (
+                  <p
+                    className="text-sm text-white bg-cyan-600 text-center w-28 border rounded"
+                  >
+                    ACTIVE MEMBER
+                  </p>
+                ) : (
+                  <p
+                    className="text-sm text-white bg-gray-600 text-center w-28 border rounded"
+                  >
+                    NOT A MEMBER
+                  </p>
+                )}
             </td>
             <th className="border border-slate-400 p-2">
-              <button type="button" className="border border-gray-400 py-2 w-28 text-base rounded">Join Mission</button>
+              {!msn.reserved
+                ? (
+                  <button
+                    onClick={() => dispatch(joiningMissionAction(msn.id))}
+                    type="button"
+                    className="border border-gray-400 py-2 w-28 text-base rounded"
+                  >
+                    Join Mission
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => dispatch(leavingMissionAction(msn.id))}
+                    type="button"
+                    className="border border-red-600 py-2 w-28 text-base rounded text-red-600"
+                  >
+                    Leave Mission
+                  </button>
+                )}
             </th>
           </tr>
         ))
--- src/redux/missions/Missions.js
+++ src/redux/missions/Missions.js
@@ -1,17 +1,38 @@
 import axios from 'axios';
+import { MISSIONS_URL } from '../../url_config';
 
 const initialState = [];
 const GET_MISSIONS = 'GET_MISSIONS';
+const JOIN_MISSION = 'JOIN_MISSION';
+const LEAVE_MISSION = 'LEAVE_MISSION';
 
-const getMissionsAction = (missions) => ({
-  type: GET_MISSIONS,
-  payload: missions,
+const getMissionsAction = (data) => {
+  const missions = data.map((mission) => ({
+    id: mission.mission_id,
+    name: mission.mission_name,
+    description: mission.description,
+    reserved: false,
+  }));
+  return {
+    type: GET_MISSIONS,
+    payload: missions,
+  };
+};
+
+export const joiningMissionAction = (id) => ({
+  type: JOIN_MISSION,
+  id,
+});
+
+export const leavingMissionAction = (id) => ({
+  type: LEAVE_MISSION,
+  id,
 });
 
 export const getMissions = () => async (dispatch) => {
   await axios({
     method: 'get',
-    url: 'https://api.spacexdata.com/v3/missions',
+    url: MISSIONS_URL,
     responseType: 'json',
   })
     .then((res) => {
@@ -23,6 +44,22 @@ const missionsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_MISSIONS:
       return [...action.payload];
+
+    case JOIN_MISSION: {
+      const newState = state.map((mission) => {
+        if (mission.id !== action.id) { return mission; }
+        return { ...mission, reserved: true };
+      });
+      return newState;
+    }
+
+    case LEAVE_MISSION: {
+      const newState = state.map((mission) => {
+        if (mission.id !== action.id) { return mission; }
+        return { ...mission, reserved: false };
+      });
+      return newState;
+    }
 
     default:
       return state;
